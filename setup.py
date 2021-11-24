e609b051e2d04ec2df86a2575066593249333c91
ads-hayashi <3213035+ads-hayashi@users.noreply.github.com>
2021-11-25 00:49:46 +0900

    テスト関連の環境調整

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    install_requires=[
+        "docker",
+        "rich[jupyter]",
+    ]
+)
